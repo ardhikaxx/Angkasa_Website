c07d5670eddc04901d58a92cc5b93de59e159702
ardhikaxx <ardhikayanuar58@gmail.com>
2023-11-04 09:11:45 +0700

    update- pada gallery

--- Gallery.php
+++ Gallery.php
@@ -4,6 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/css/lightbox.min.css">
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     <link rel="icon" type="image/png" href="assets/Logo Web.png">
     <title>Angkasa | Gallery Page</title>
@@ -12,6 +15,28 @@
             padding: 0;
             margin: 0;
             box-sizing: border-box;
+        }
+
+        html {
+            scroll-behavior: smooth;
+        }
+
+        ::-webkit-scrollbar {
+            width: 10px;
+            border-radius: 50px;
+        }
+
+        ::-webkit-scrollbar-track {
+            background: #f1f1f1;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: #ccc;
+            border-radius: 30px;
+        }
+
+        ::-webkit-scrollbar-thumb:hover {
+            background: #bbb;
         }
 
         .navbar {
@@ -572,6 +597,48 @@
                 opacity: 0.6;
             }
         }
+
+        .photo-gallery {
+            color: #313437;
+            background-color: #fff;
+        }
+
+        .photo-gallery p {
+            color: #7d8285;
+        }
+
+        .photo-gallery h2 {
+            font-weight: bold;
+            margin-bottom: 40px;
+            padding-top: 40px;
+            color: inherit;
+        }
+
+        @media (max-width:767px) {
+            .photo-gallery h2 {
+                margin-bottom: 25px;
+                padding-top: 25px;
+                font-size: 24px;
+            }
+        }
+
+        .photo-gallery .intro {
+            font-size: 16px;
+            max-width: 500px;
+            margin: 0 auto 40px;
+        }
+
+        .photo-gallery .intro p {
+            margin-bottom: 0;
+        }
+
+        .photo-gallery .photos {
+            padding-bottom: 20px;
+        }
+
+        .photo-gallery .item {
+            padding-bottom: 30px;
+        }
     </style>
 </head>
 
@@ -599,6 +666,21 @@
         <div class="pack-title">
             <h1>Gallery</h1>
             <p>Here's an example of the results from our photos</p>
+        </div>
+    </div>
+
+    <div class="photo-gallery">
+        <div class="container mt-5">
+            <div class="row photos" data-aos="fade-down" data-aos-easing="ease" data-aos-duration="500">
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto1.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto1.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto2.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto2.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto3.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto3.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto4.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto4.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto5.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto5.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto6.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto6.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto7.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto7.jpg"></a></div>
+                <div class="col-sm-6 col-md-4 col-lg-3 item"><a href="assets/Gallery/foto8.jpg" data-lightbox="photos"><img class="img-fluid" style="border-radius: 15px;" src="assets/Gallery/foto8.jpg"></a></div>
+            </div>
         </div>
     </div>
 
@@ -655,6 +737,10 @@
         <div class="star"></div>
     </div>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.8.2/js/lightbox.min.js"></script>
 
     <script>
         AOS.init();
